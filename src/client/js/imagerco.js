44644adf2b6d03925ba68d66aa4f546854ad7330
Zeuxis Lo <seekstudio@gmail.com>
2014-10-15 01:17:19 +0800

    add first prototype

---
+++ src/client/js/imagerco.js
@@ -0,0 +1,78 @@
+var dataURItoBlob = function(dataURI) {
+    var byteString;
+
+    if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+        byteString = atob(dataURI.split(',')[1]);
+    }else{
+        byteString = unescape(dataURI.split(',')[1]);
+    }
+
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
+
+chrome.contextMenus.create({
+    "title": "ImageRCO -----> --->",
+    "contexts": ["image"],
+    "onclick": function(info, tab) {
+        chrome.storage.sync.get({
+            debug : true,
+            apiKey: ''
+        }, function(options) {
+            console.log(options);
+            var debug      = options.debug;
+            var apiKey     = options.apiKey;
+
+            var ocrService = 'http://api.ocrapiservice.com/1.0/rest/ocr';
+            var srcUrl     = info.srcUrl;
+            var formData   = new FormData();
+
+            if (debug === true) {
+                ocrService = 'http://localhost:8080/index.php';
+            }
+
+            console.log('API KEY : %s', apiKey);
+            console.log('Debug   : %s', debug ? 'YES' : 'NO');
+            console.log('OCR     : %s', ocrService);
+
+            console.log('Action  : FormData');
+
+            formData.append('image', dataURItoBlob(srcUrl), 'someName.png');
+            formData.append('language', 'en');
+            formData.append('apikey', apiKey);
+
+            console.log('Action  : AJAX');
+
+            $.ajax({
+                type       : 'POST',
+                url        : ocrService,
+                data       : formData,
+                // dataType   : 'JSON',
+                processData: false,
+                contentType: false
+            }).success(function(response) {
+                console.log('Action  : AJAX Success');
+                console.log('Response:');
+                console.log('==>');
+                console.log(response);
+
+                var code = response.split(':')[1].trim();
+
+                window.prompt('Success! Your OCR code', code);
+            }).fail(function(response) {
+                console.log('Action  : AJAX Failed');
+                console.log('Response:');
+                console.log('==>');
+                console.log(response);
+
+                window.prompt('Failed! Response message', response.responseText);
+            });
+        });
+    }
+});
---
+++ src/client/js/options.js
@@ -0,0 +1,34 @@
+;(function($) {
+
+    $(function() {
+        chrome.storage.sync.get({
+            debug : false,
+            apiKey: ''
+        }, function(items) {
+            console.log(items);
+            $('#debug').prop('checked', items.debug);
+            $('#api-key').val(items.apiKey);
+        });
+
+
+        $('#save-button').click(function() {
+            var apiKey = $("#api-key").val();
+            var debug  = $("#debug").is(":checked");
+
+            if (apiKey === "") {
+                alert('Please enter API key');
+                return;
+            }
+
+            console.log(debug);
+
+            chrome.storage.sync.set({
+                debug : debug,
+                apiKey: apiKey
+            }, function() {
+                $('.message-block').text('Saved!').fadeOut(1200);
+            });
+        });
+    });
+
+})(jQuery);
